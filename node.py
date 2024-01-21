f505ab3760536d5c15c382a69d221c88c8d077d0
mpasche <martin.pasche@uc.cl>
2024-01-21 14:32:45 +0100

    20-01

--- node.py
+++ node.py
@@ -38,12 +38,11 @@ class Node:
     def get_lambda_interval(self, verbose = False):
         self.verbose = verbose
         
-        self.omega = 0.5 * np.linalg.norm(self.y - self.A @ self.x)**2 - \
+        self.omega = float( 0.5 * np.linalg.norm(self.y - self.A @ self.x)**2 - \
             0.5 * np.linalg.norm(self.y)**2 +  \
-            0.5 * np.linalg.norm(self.y-self.u)**2
-            
+            0.5 * np.linalg.norm(self.y-self.u)**2 )
         for i in self.S1:
-            self.omega += self.M*abs(self.A[:, i] @ self.u)
+            self.omega += float( self.M*abs(self.A[:, i] @ self.u) )
 
         class Au:
             def __init__(self, value, index):
@@ -57,12 +56,25 @@ class Node:
         # decreasing
         test = sorted([Au(self.M*abs(self.A[:, i] @ self.u), i)
                        for i in self.S], reverse=True, key=lambda x: x.value)
+        
+        
 
         def calc_bound_leaf_node ():
             constant1 = self.omega
-            constant2 = len(self.S1) - np.count_nonzero(self.x)
+            constant2 = float( len(self.S1) - np.count_nonzero(self.x) )
             if constant2 != 0:
                 value =  constant1 / constant2
+                if 0 > constant2:
+                    # negative
+                    self.show("Condition to refute: lambda* < ", round(value, 3) )
+                    possible_lower_bound = value
+                    possible_upper_bound = np.inf
+                else:
+                    # positive
+                    self.show("Condition to refute:", round(value, 3), "< Lambda*")
+                    possible_upper_bound = value
+                    possible_lower_bound = -np.inf
+                return (possible_lower_bound, possible_upper_bound)
             else: 
                 if self.omega < 0:
                     print("Omega:", self.omega)
@@ -72,39 +84,68 @@ class Node:
             
         
         def calc_bound(j):
-            constant1 = self.omega + \
-                np.sum([test[i].value for i in range(j + 1)])
-            constant2 = len(self.S1)-np.count_nonzero(self.x)+j+1
+            constant1 = float( self.omega + \
+                np.sum([test[i].value for i in range(j + 1)]) )
+            constant2 = float( len(self.S1)-np.count_nonzero(self.x)+j+1 )
             
             if constant2 != 0:
-
-                if not 0 <= constant2:
+                value = constant1 / constant2
+                if 0 > constant2:
                     # negative
-                    possible_lower_bound = constant1 / constant2
+                    self.show("Condition to refute: Lambda* <", round(value, 3))
+                    possible_lower_bound = value
                     possible_upper_bound = np.inf
                 else:
                     # positive
-                    possible_upper_bound = constant1 / constant2
+                    self.show("Condition to refute:", round(value, 3), "< Lambda*")
+                    possible_upper_bound = value
                     possible_lower_bound = -np.inf
                 return (possible_lower_bound, possible_upper_bound)
             
             else:
                 #when constant2 is zero we cannot check the condition
+                if constant1 < 0:
+                    print("Omega + Sum M|ai u|:", constant1)
+                    raise Exception("The sum of omega and sumatory is negative when the refute is at Omega + Sum < lambda * 0") 
+                else:
+                    self.show("No condition, we have Omega + Sum > 0") 
                 return (-np.inf, np.inf)
             
         
-        def check_if_better_bound (possible_lb, possible_ub, lower_bound, upper_bound):
+        def check_if_better_bound (j, possible_lb, possible_ub, lower_bound, upper_bound):
             if possible_lb == -np.inf:
-                if possible_ub < test[j].value:
+                if not (j == -1 or j == len(test) - 1 ):
+                    if possible_ub < test[j].value:
+                        self.show("Update upper bound")
+                        upper_bound = min(upper_bound, possible_ub)
+                else:
+                    self.show("Lambda is", self.lambda0, "and the possible upper bound is", possible_ub)
+                    if possible_ub < self.lambda0:
+                        raise Exception("Imposible condition: possible ub < lambda")
+                    ex_upper = upper_bound
                     upper_bound = min(upper_bound, possible_ub)
+                    if ex_upper != upper_bound:
+                        self.show("Upper bound changed!")
+                        self.show("[", lower_bound, ",", upper_bound, "]")
             
             if possible_ub == np.inf:
-                if possible_lb > test[j].value:
-                    lower_bound = max(lower_bound, possible_lb)
+                if not (j == len(test) - 1 or j == -1):
+                    if possible_lb > test[j+1].value:
+                        self.show("Update lower bound")
+                        lower_bound = max(lower_bound, possible_lb)
+                else:
+                    self.show("Lambda is", self.lambda0, "and the possible lower bound is", possible_lb)
+                    if possible_ub < self.lambda0:
+                        raise Exception("Imposible condition: lambda < possible lb")
+                    ex_lower = lower_bound
+                    lower_bound = min(lower_bound, possible_ub)
+                    if ex_lower != lower_bound:
+                        self.show("Change lower bound!")
+                        self.show("[", lower_bound, ",", upper_bound, "]")
                     
             return lower_bound, upper_bound
             
-            
+    
         
 
         
@@ -117,6 +158,7 @@ class Node:
         self.show(test)
         self.show("omega:", self.omega)
         self.show("J0:", j0)
+        self.show("Lambda:", self.lambda0)
         
         
         # If we are in the solution node
@@ -126,8 +168,8 @@ class Node:
         else:   
             if j0 == len(self.S) - 1:
                 self.show("First interval", "[", -np.inf, ",", test[j0].value, "]")
-            elif j0 == 0:
-                self.show("First interval", "[", test[j0].value, ",", np.inf, "]")
+            elif j0 == -1:
+                self.show("First interval", "[", test[0].value, ",", np.inf, "]")
             else:
                 self.show("First interval", "[", test[j0 + 1].value, ",", test[j0].value, "]")
             
@@ -137,38 +179,38 @@ class Node:
             for j in range(j0, len(test)):
                 self.show("J:", j)
                 if j == len(self.S) - 1:
-                    self.show("[", -np.inf, ",", test[j].value, "]")
-                elif j == 0:
-                    self.show("[", test[j].value, ",", np.inf, "]")
+                    self.show("If Lambda* in [", -np.inf, ",", test[j].value, "]")
+                elif j == -1:
+                    self.show("If Lambda* in [", test[0].value, ",", np.inf, "]")
                 else:
-                    self.show("[", test[j + 1].value, ",", test[j0].value, "]")
+                    self.show("If Lambda* in [", test[j + 1].value, ",", test[j].value, "]")
                 
                 possible_lower_bound, possible_upper_bound = calc_bound(j)
-                lower_bound, upper_bound = check_if_better_bound(
+                lower_bound, upper_bound = check_if_better_bound(j,
                     possible_lower_bound, possible_upper_bound, 
                     lower_bound, upper_bound
                 )
-                #self.show("lower:", possible_lower_bound, "upper:", possible_upper_bound)
+                self.show("Possible interval: [", possible_lower_bound, ", ", possible_upper_bound, "]")
 
             
             #now the other set of intervals
-            for j in range(j0, -1, -1):
+            for j in range(j0, -2, -1):
                 self.show("J:", j)
                 if j == len(self.S) - 1:
-                    self.show("[", -np.inf, ",", test[j].value, "]")
-                elif j == 0:
-                    self.show("[", test[j].value, ",", np.inf, "]")
+                    self.show("If Lambda* in [", -np.inf, ",", test[j].value, "]")
+                elif j == -1:
+                    self.show("If Lambda* in [", test[j].value, ",", np.inf, "]")
                 else:
-                    self.show("[", test[j + 1].value, ",", test[j0].value, "]")
+                    self.show("If Lambda* in [", test[j + 1].value, ",", test[j].value, "]")
                 
                 possible_lower_bound, possible_upper_bound = calc_bound(j)
-                lower_bound, upper_bound = check_if_better_bound(
+                lower_bound, upper_bound = check_if_better_bound(j,
                     possible_lower_bound, possible_upper_bound, 
                     lower_bound, upper_bound
                 )
-                #self.show("lower:", possible_lower_bound, "upper:", possible_upper_bound)
+                self.show("Possible interval: [", possible_lower_bound, ", ", possible_upper_bound, "]")
 
-        self.show("[", lower_bound, ",", upper_bound, "]")
+        self.show(self,"[", lower_bound, ",", upper_bound, "]", "\n\n")
         self.lambda_lb, self.lambda_ub = lower_bound, upper_bound
         return lower_bound, upper_bound
 
